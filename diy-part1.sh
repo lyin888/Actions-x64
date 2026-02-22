3dd9e2ff0d6166db35b459e4d9a901553bf8138c
lyin888 <lyin007@gmail.com>
2026-02-22 13:58:45 +0800

    升级配置文件

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,8 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >>feeds.conf.default
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
+echo 'src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main' >>feeds.conf.default
+echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' >>feeds.conf.default
 echo 'src-git gecoosac https://github.com/lwb1978/openwrt-gecoosac;main' >>feeds.conf.default
 echo 'src-git aliyundrive_webdav https://github.com/messense/aliyundrive-webdav;main' >>feeds.conf.default
 echo 'src-git lyin_diy https://github.com/lyin888/lyin_diy;main' >>feeds.conf.default
