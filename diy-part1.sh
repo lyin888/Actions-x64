d6319dfb2b2e3b1262ce60dbb14aa9d33be7af8d
lyin888 <lyin007@gmail.com>
2026-03-15 12:09:01 +0800

    更新配置文件

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,9 +14,10 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >>feeds.conf.default
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
-echo 'src-git gecoosac https://github.com/lwb1978/openwrt-gecoosac;main' >>feeds.conf.default
-echo 'src-git aliyundrive_webdav https://github.com/messense/aliyundrive-webdav;main' >>feeds.conf.default
-echo 'src-git lyin_diy https://github.com/lyin888/lyin_diy;main' >>feeds.conf.default
-echo 'src-git OpenAppFilter https://github.com/destan19/OpenAppFilter' >>feeds.conf.default
+echo 'src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git' >>feeds.conf.default
+echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git' >>feeds.conf.default
+echo 'src-git gecoosac https://github.com/lyin888/openwrt-gecoosac.git' >>feeds.conf.default
+echo 'src-git aliyundrive_webdav https://github.com/messense/aliyundrive-webdav.git' >>feeds.conf.default
+echo 'src-git lyin_diy https://github.com/lyin888/lyin_diy.git' >>feeds.conf.default
+echo 'src-git OpenAppFilter https://github.com/destan19/OpenAppFilter.git' >>feeds.conf.default
+echo 'src-git rtp2httpd https://github.com/stackia/rtp2httpd.git' >>feeds.conf.default
