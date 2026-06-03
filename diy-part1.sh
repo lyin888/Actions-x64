172b0a0f6a0a64f24260c86a7b3539b2c766bf07
lyin888 <lyin007@gmail.com>
2026-06-03 11:45:35 +0800

    更新配置文件

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
+echo 'src-git packages https://github.com/lyin888/packages-25.12.0.git' >>feeds.conf.default
+echo 'src-git luci https://github.com/lyin888/luci-25.12.0.git' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git' >>feeds.conf.default
 echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git' >>feeds.conf.default
 echo 'src-git gecoosac https://github.com/lyin888/openwrt-gecoosac.git' >>feeds.conf.default
@@ -21,3 +23,4 @@ echo 'src-git aliyundrive_webdav https://github.com/messense/aliyundrive-webdav.
 echo 'src-git lyin_diy https://github.com/lyin888/lyin_diy.git' >>feeds.conf.default
 echo 'src-git OpenAppFilter https://github.com/destan19/OpenAppFilter.git' >>feeds.conf.default
 echo 'src-git rtp2httpd https://github.com/stackia/rtp2httpd.git' >>feeds.conf.default
+echo 'src-git tailscale_community https://github.com/Tokisaki-Galaxy/luci-app-tailscale-community.git' >>feeds.conf.default
